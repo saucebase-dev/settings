20c12a0d6faf4c32b2038f27ee5078fbcaa44140
roble <renanroble@gmail.com>
2026-01-20 21:07:34 +0000

    feat: add navigation configuration for Settings module

---
+++ routes/navigation.php
@@ -0,0 +1,54 @@
+<?php
+
+use Spatie\Navigation\Facades\Navigation;
+use Spatie\Navigation\Section;
+
+/*
+|--------------------------------------------------------------------------
+| Settings Module Navigation
+|--------------------------------------------------------------------------
+|
+| Define Settings module navigation items here.
+| These items will be loaded automatically when the module is enabled.
+|
+*/
+
+// User menu - Settings
+Navigation::add('Settings', route('settings.index'), function (Section $section) {
+    $section->attributes([
+        'group' => 'user',
+        'slug' => 'settings',
+        'order' => 10,
+    ]);
+});
+
+// Settings sidebar - General
+Navigation::add('General', route('settings.index'), function (Section $section) {
+    $section->attributes([
+        'group' => 'settings',
+        'slug' => 'settings',
+        'order' => 10,
+    ]);
+});
+
+// Settings sidebar - Profile
+Navigation::add('Profile', route('settings.profile'), function (Section $section) {
+    $section->attributes([
+        'group' => 'settings',
+        'slug' => 'profile',
+        'order' => 20,
+    ]);
+});
+
+// Secondary navigation - Settings
+Navigation::add('Settings', route('settings.index'), function (Section $section) {
+    $section->attributes([
+        'group' => 'secondary',
+        'slug' => 'settings',
+        'order' => 10,
+        'badge' => [
+            'content' => '1',
+            'variant' => 'destructive',
+        ],
+    ]);
+});
