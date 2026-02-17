da2ff6b52fadc72b7166c747dfbb9e385c355894
roble <renanroble@gmail.com>
2026-02-17 20:15:43 +0000

    fix: update ExampleTest to expect no assertions instead of asserting true

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -11,6 +11,6 @@ class ExampleTest extends TestCase
 
     public function test_that_true_is_true()
     {
-        $this->assertTrue(true);
+        $this->expectNotToPerformAssertions();
     }
 }
